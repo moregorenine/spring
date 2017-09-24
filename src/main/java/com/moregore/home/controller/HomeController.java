f669177e505e5164f76295ad921fa48a352100ae
moregorenine <moregorenine@gmail.com>
2017-09-25 01:11:18 +0900

    log 메시지 기능 추가

--- src/main/java/com/moregore/home/controller/HomeController.java
+++ src/main/java/com/moregore/home/controller/HomeController.java
@@ -1,15 +1,20 @@
-package com.moregore.spring.home.controller;
+package com.moregore.home.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping("/main")
 	public String home() {
+		log.debug("start : /home/main");
 		return "/home/main";
 	}
-	
+
 }
