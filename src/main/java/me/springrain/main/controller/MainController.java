5db6d7c229ce222c54fca5a7c8815b516887c924
moregorenine <moregorenine@gmail.com>
2017-09-28 22:04:07 +0900

    main page html 수정

--- src/main/java/me/springrain/main/controller/MainController.java
+++ src/main/java/me/springrain/main/controller/MainController.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/main")
 public class MainController {
 
 	private static final Logger log = LoggerFactory.getLogger(MainController.class);
 
-	@RequestMapping("")
+	@RequestMapping("/main.do")
 	public String home() {
 		log.debug("welcome to springrain.me main page");
 		return "/main";
--- webapp/index.jsp
+++ webapp/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-hello index.jsp
+hello index.jsp page
 </body>
 </html>
