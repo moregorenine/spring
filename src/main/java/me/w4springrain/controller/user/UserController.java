c1838a917bcb985604146a47afa0025defd26150
moregorenine <moregorenine@gmail.com>
2017-10-12 00:47:33 +0900

    swaggerconfig 추가

--- src/main/java/me/w4springrain/controller/user/UserController.java
+++ src/main/java/me/w4springrain/controller/user/UserController.java
@@ -10,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import me.w4springrain.dao.user.UserDao;
 import me.w4springrain.domain.user.User;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/user")
 @Transactional
 public class UserController {
 
@@ -23,7 +28,13 @@ public class UserController {
 	@Autowired
 	private UserDao userDao;
 	
-	@RequestMapping(value="/create", method=RequestMethod.POST)
+	@RequestMapping(method = RequestMethod.POST, path = "", produces = "application/json")
+	@ApiOperation(value = "getGreeting", nickname = "getGreeting")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "name", value = "User's name", required = false, dataType = "string", paramType = "query", defaultValue = "Niklas") })
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success", response = User.class), @ApiResponse(code = 401, message = "Unauthorized"),
+			@ApiResponse(code = 403, message = "Forbidden"), @ApiResponse(code = 404, message = "Not Found"),
+			@ApiResponse(code = 500, message = "Failure") }) 
 	public User create(@Valid User user) {
 		log.debug("User : {}", user);
 		int cntCreate = userDao.create(user);
