e976383e532136f7f11c9483485690a7e9ebff38
moregorenine <moregorenine@gmail.com>
2017-11-02 21:41:52 +0900

    swagger-ui 메뉴 추가

--- src/main/java/me/w4springrain/main/controller/MainController.java
+++ src/main/java/me/w4springrain/main/controller/MainController.java
@@ -1,4 +1,4 @@
-package me.w4springrain.controller.main;
+package me.w4springrain.main.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
--- src/test/java/me/w4springrain/main/controller/MainControllerTest.java
+++ src/test/java/me/w4springrain/main/controller/MainControllerTest.java
@@ -1,13 +1,16 @@
-package me.w4springrain.controller.main;
+package me.w4springrain.main.controller;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import me.w4springrain.main.controller.MainController;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/applicationContext.xml")
