2f7d835e9cf0b764b5099908dfb6ea7d7a2d0d01
moregorenine <moregorenine@gmail.com>
2018-11-15 23:18:01 +0900

    init

---
+++ tree-structure-ztree/src/test/java/com/github/moregorenine/TreeStructureZtreeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.moregorenine;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TreeStructureZtreeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
