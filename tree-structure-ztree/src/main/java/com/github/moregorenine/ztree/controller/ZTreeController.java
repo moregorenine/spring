281fcf42cdbfab9d3b68b89769a036816cac369d
moregorenine <moregorenine@gmail.com>
2018-11-19 23:13:13 +0900

    derby db -> h2 db로 변경 & ztree plugin 추가

--- tree-structure-ztree/src/main/java/com/github/moregorenine/ztree/controller/ZTreeController.java
+++ tree-structure-ztree/src/main/java/com/github/moregorenine/ztree/controller/ZTreeController.java
@@ -3,21 +3,24 @@ package com.github.moregorenine.ztree.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.github.moregorenine.ztree.domain.ZTree;
 
-@RestController
+@Controller
 public class ZTreeController {
 	
 	
 	@Autowired
 	ZTreeService zTreeService;
 
-	@RequestMapping("/ztrees")
-	public List<ZTree> getAllZTrees() {
-		return zTreeService.getAllZTrees();
+	@RequestMapping("ztrees")
+	public String  getAllZTrees(Model model) {
+		List<ZTree> ztrees = zTreeService.getAllZTrees();
+		model.addAttribute(ztrees);
+		return "ztree/ztrees";
 	}
 
 }
